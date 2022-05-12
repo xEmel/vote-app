9f8954d20935c9db75f4dcce9fc98f0b94208d8d
Kamil <tashanek@gmail.com>
2022-05-12 18:10:52 +0200

    Add material-ui and base config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { globalTheme } from './theme/global-theme.jss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={globalTheme}>
+      <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/theme/global-theme.jss.ts
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material/styles';
+
+export const globalTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+});
